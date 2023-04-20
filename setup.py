9cbcc4e6a0a5dc43b7d81099b3589164aa987efa
Roman Ellerbrock <re@terraquantum.swiss>
2023-04-20 20:51:46 +0200

    initial commit.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='OpenTN',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://github.com/roman-ellerbrock/OpenTN',
+    license='MIT',
+    author='Roman Ellerbrock',
+    author_email='romanellerbrock@gmail.com',
+    description='A Python package for implementing MERA',
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy',
+        'scipy',
+        'tqdm',
+        # add any additional dependencies here
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
